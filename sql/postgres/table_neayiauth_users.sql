4b7250dfc091d2e128f30a6a5bc83241a016a234
Bertrand Gorge <bertrand.gorge@neayi.com>
2021-04-09 16:03:23 +0200

    Added neayiauth_external_apitoken

--- sql/postgres/table_neayiauth_users.sql
+++ sql/postgres/table_neayiauth_users.sql
@@ -1,4 +1,5 @@
 CREATE TABLE neayiauth_users (
     neayiauth_user INTEGER NOT NULL PRIMARY KEY,
-    neayiauth_external_userid INTEGER NOT NULL UNIQUE
+    neayiauth_external_userid INTEGER NOT NULL UNIQUE,
+    neayiauth_external_apitoken VARCHAR(255) UNIQUE NOT NULL
 );
--- sql/sqlite/table_neayiauth_users.sql
+++ sql/sqlite/table_neayiauth_users.sql
@@ -1,4 +1,5 @@
 CREATE TABLE /*_*/neayiauth_users (
     neayiauth_user int unsigned PRIMARY KEY NOT NULL,
-    neayiauth_external_userid  int unsigned UNIQUE NOT NULL
+    neayiauth_external_userid  VARCHAR(255) UNIQUE NOT NULL,
+    neayiauth_external_apitoken VARCHAR(255) UNIQUE NOT NULL
 ) /*$wgDBTableOptions*/;
